d8efa2c4563ada02a4fd239bf9f38fee708a43ec
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-10-02 15:51:16 +0600

    authenticated request with prepareHeades in fetch base query

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -6,7 +6,17 @@ export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
         // baseUrl: 'http://localhost:9000'
-        baseUrl: process.env.REACT_APP_API_URL
+        baseUrl: process.env.REACT_APP_API_URL,
+        prepareHeaders: async (headers, { getState, endpoint }) => {
+            const token = getState()?.auth.accessToken
+
+            if (token) {
+                headers.set("Authorization", `Bearer ${token}`)
+            }
+
+            return headers
+        }
+
     }),
     tagTypes: [],
     endpoints: () => ({})
