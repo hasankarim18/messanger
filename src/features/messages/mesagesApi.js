39a08d98adb53306bff99e1e6433a3abb2bcf117
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-10-05 17:48:29 +0600

    messageApi completed and chathead

--- src/features/messages/mesagesApi.js
+++ src/features/messages/mesagesApi.js
@@ -3,6 +3,12 @@ import { apiSlice } from "../api/apiSlice";
 
 const messagesApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-
+        getMessages: builder.query({
+            query: (id) => `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit=${process.env.REACT_APP_MESSAGES_PER_PAGE}`
+        })
     })
 })
+
+export const {
+    useGetMessagesQuery
+} = messagesApi
