7eb12ae256bf838edb4cfba9e80183e3b53969fd
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-10-08 15:42:08 +0600

    debounce entered email

--- src/components/inbox/Navigation.js
+++ src/components/inbox/Navigation.js
@@ -1,11 +1,17 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import logoImage from "../../assets/images/lws-logo-dark.svg";
 import { userLogeedOut } from "../../features/auth/authSlice";
+import getAvater from 'gravatar-url'
 
 export default function Navigation() {
 
     const dispatch = useDispatch()
+
+    const { user } = useSelector(state => state.auth) || {}
+    const { email, name } = user || {}
+
+
 
     const logoutHandler = () => {
         dispatch(userLogeedOut())
@@ -17,13 +23,18 @@ export default function Navigation() {
         <nav className="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
             <div className="max-w-7xl mx-auto">
                 <div className="flex justify-between h-16 items-center">
-                    <Link to="/">
-                        <img
-                            className="h-10"
-                            src={logoImage}
-                            alt="Learn with Sumit"
-                        />
-                    </Link>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                        <Link to="/">
+                            <img
+                                className="h-10"
+                                src={getAvater(email, 80)}
+                                alt="Learn with Sumit"
+                            />
+                        </Link>
+                        <div style={{ textTransform: "capitalize", marginLeft: "10px", fontSize: "24px", color: "#fff" }} >
+                            {name}
+                        </div>
+                    </div>
                     <ul>
                         <li className="text-white">
                             <span
