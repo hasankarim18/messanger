e824aa32880a62740d0bbafa6eac101182cef8d5
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-10-10 11:18:26 +0600

    user check

--- src/components/inbox/Modal.js
+++ src/components/inbox/Modal.js
@@ -1,17 +1,25 @@
 import { useState } from "react";
+import { useGetUserQuery } from "../../features/users/usersApi";
 import IsEmailValid from "../../utils/IsEmailValid";
+import Error from '../ui/Error'
 
 export default function Modal({ open, control }) {
 
     const [to, setTo] = useState('')
     const [message, setMessage] = useState('')
+    const [userCheck, setUserCheck] = useState(false)
 
+    const { data: participant } = useGetUserQuery(to, {
+        skip: !userCheck
+    })
+    console.log(participant)
     const debounceHandler = (fn, delay) => {
         let timeoutId;
         return (...arg) => {
             clearTimeout(timeoutId)
             timeoutId = setTimeout(() => {
                 fn(...arg)
+
             }, delay);
         }
     }
@@ -20,8 +28,7 @@ export default function Modal({ open, control }) {
         if (IsEmailValid(value)) {
             // chcck use api
             setTo(value)
-
-
+            setUserCheck(true)
         }
 
     }
@@ -79,6 +86,7 @@ export default function Modal({ open, control }) {
 
                         <div>
                             <button
+
                                 type="submit"
                                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
                             >
@@ -86,7 +94,7 @@ export default function Modal({ open, control }) {
                             </button>
                         </div>
 
-                        {/* <Error message="There was an error" /> */}
+                        {participant?.length === 0 && <Error message="This user does not exists!" />}
                     </form>
                 </div>
             </>
